c1b0c75c6c3054ed81b2e25ed35828c2c292bda1
Sanjeevvarmabetter <131432196+Sanjeevvarmabetter@users.noreply.github.com>
2023-07-03 06:38:58 +0530

    Create map.cpp

---
+++ array/map.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin >> n;
+	int arr[n];
+	
+	for(int i=0;i<n;i++) {
+		cin >> arr[i];
+	}
+	
+	//precomputation using a map
+	
+	map<int,int> m;		//this is how we declare a map
+	for(int i=0;i<n;i++) {
+		m[arr[i]]++;
+	 }
+	 
+	 int a;
+	 cin >> a;
+	 
+	 while(a--) {
+		 int q;
+		 cin >> q;
+		 
+		 cout << m[q] << endl;
+	
+	}
+	return 0;
+}
