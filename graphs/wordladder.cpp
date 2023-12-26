8afc692871835e4e64c8a45943e7fc0260f0d254
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2023-12-26 23:49:18 +0530

    added hard problem

---
+++ graphs/wordladder.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        set<int> st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+
+        while(!q.empty()) {
+
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+        for(int i=0;i<word.size();i++) {
+                char original = word[i];
+            for(char c = 'a';c<'z';c++) {
+                word[i] = c;
+            }
+
+            //if the word exists
+
+            if(st.find(word) != st.end()) {
+                st.erase(word);
+                q.push({word, steps + 1});
+            }
+            word[i] = original;
+
+        }
+
+
+
+        }
+        return 0;
+    }
+};
