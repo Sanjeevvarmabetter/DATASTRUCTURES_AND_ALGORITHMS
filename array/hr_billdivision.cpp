35b9c9335f134c04b4bbced28be4ea8ba2a32f8f
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2024-01-26 16:52:13 +0530

    all test cases passed

--- array/hr_billdivision.cpp
+++ array/hr_billdivision.cpp
@@ -6,8 +6,8 @@ class Solution {
 
     public:
 
-    void bonAppetit(vector<int> bill,int k,int b) {
-    
+    void  bonAppetit(vector<int> bill,int k,int b) {
+        //this is the initial wrong not optimized for large number of test cases
         int sum = 0;
         int b_actual;
         for(int i = 0;i<bill.size();i++) {
@@ -26,4 +26,24 @@ class Solution {
         }
     }
 
-}
+
+    void correct_bonappetit(vector<int> bill,int k, int b) {
+        int sum = 0;
+        for(int  i=0;i<bill.size();i++) {
+            if(i != k) {
+                sum  = sum + bill[i];
+
+            }
+        }
+        int b_actual = sum / 2;
+
+        if(b > b_actual) {
+            cout << b-b_actual << endl;
+        }
+
+        else {
+            cout << "Bon Appetit" << endl;
+        }
+
+    }
+};
