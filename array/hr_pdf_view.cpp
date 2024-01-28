396e1ea59f5ceb5326d0408dc9ce556cc2c219ed
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2024-01-28 12:54:04 +0530

    now perform the word operation

--- array/hr_pdf_view.cpp
+++ array/hr_pdf_view.cpp
@@ -1,1 +1,26 @@
 // code this 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+    public:
+    int designpdfviewer(vector<int> h,string word) {
+        //we need biggest number * number of elements for the word
+        //just by finding the biggest number we can do this
+        string all_alphabits = "abcdefghijklmnopqrstuvwxyz";
+        map<char,int> mp;
+
+        for(int i=0;i<h.size();i++) {
+            mp[all_alphabits[i]] = h[i];
+        }
+        
+        //now we need to read the word
+
+        
+
+    }
+};
+
