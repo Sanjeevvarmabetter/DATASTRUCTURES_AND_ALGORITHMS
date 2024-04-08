3f6fd4d1147d009270b19d4e9e6fff23b6698065
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-04-09 00:11:33 +0530

    wrong logic fix this

---
+++ HACKERRANK/tempCodeRunnerFile.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class save_prisoner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            // n = 4,m=6,s=2  
+
+            // 2 3 4 1 2 3 
+        int n = sc.nextInt(); //4
+        int m = sc.nextInt(); //6
+        int s = sc.nextInt(); //2
+
+
+        int ans =  save(n,m,s);
+
+        System.out.println(ans);
+    }
+
+
+    public static int save(int n,int m,int s) {
+        if(m<n) {
+            return m;
+        }
+        if (n==s) {
+            return n;
+        }
+        
+        int counter = s;
+
+        for(int i=s;i<=m;i++) {
+            counter++;
+            if(counter > n) {
+                counter = 1;
+            }
+        }
+        return counter;
+    }
+}
