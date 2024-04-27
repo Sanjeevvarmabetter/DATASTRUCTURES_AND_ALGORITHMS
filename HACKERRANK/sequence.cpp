79d89497f78d83f2dba98835c8a1534d1f153466
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-04-28 00:12:57 +0530

    wrong logic

---
+++ HACKERRANK/sequence.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    public:
+    int cloud_energy(int arr[],int n,int k) {
+        int energy = 100;
+        int current = 0;
+
+        for(int i=0;i<n;i+=k) {
+            current = i;
+            energy = energy - 1;
+            if(arr[current] == 1) {
+                energy = energy - 2;
+
+            }
+
+        }
+        return energy;
+    }
+};
+
+int main() {
+    int n,k;
+    cin >> n;   
+    cin >> k;
+    int arr[n];
+
+    for(int i=0;i<k;i++) {
+        cin >> arr[i];
+    }
+
+    Solution sol;
+    int ans = sol.cloud_energy(arr,n,k);
+    cout << ans << endl;
+    return 0;
+
+}
