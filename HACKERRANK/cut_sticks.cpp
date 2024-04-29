75cc207becd88ea7a1f61e5f836053daa50f8355
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-04-29 17:58:55 +0530

    fix issues

---
+++ HACKERRANK/cut_sticks.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+    public:
+    int cut(vector<int> arr,int k) {
+        sort(arr.begin(),arr.end());
+    vector<int> counts;
+
+    while(!arr.empty()) {
+        int length_of_array = arr.size();
+
+        counts.push_back(length_of_array);
+        int s = shortest(arr);
+        for(int i=0;i<arr.size();i++) {
+            arr[i] = arr[i] - s;
+            if(arr[i] <= 0) {
+                arr.remove(i);
+            }
+        }
+
+    }
+    }
+
+    int shortest(vector<int> ans) {
+        int n = ans.size();
+        int min = ans[0];
+        for(int i=0;i<n;i++) {
+            if(ans[i]< min) {
+                min = ans[i];
+            }
+        }
+        return min;
+    }
+};
+
+
+int main() {
+
+    vector<int> arr;
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++) {
+        cin >> arr[i];
+
+    }
+    int k;
+    cin >> k;
+
+    Solution sol;
+    int ans = sol.cut(arr,k);
+    cout << ans << endl; 
+}
