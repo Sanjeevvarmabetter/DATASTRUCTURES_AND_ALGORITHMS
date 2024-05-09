a56604fa44f2b88918fa056cd507c89aa4c64ec0
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-05-09 17:07:30 +0530

    fixeed

--- HACKERRANK/library_fine.cpp
+++ HACKERRANK/library_fine.cpp
@@ -22,23 +22,21 @@ class Solution {
     public:
 
     int library_fees(int d1,int m1,int y1,int d2,int m2,int y2) {
-        if(y1>y2) {
-            return 1000;
+
+        if(d1>d2 && m1==m2 && y1==y2) {
+            return (d1-d2)*15;
         }
 
-        if(m1>m2 && y1==y2) {
-            return 500 * (m1-m2);
-        }
-        if(d1>d2 && m1==m2) {
-            return 15 * (d1-d2);
+        else if(m1>m2 && y1==y2) {
+            return (m1-m2) * 500;
         }
 
-        if(d1>d2 && m1>m2) {
-            return 15*(d1-d2) + 500 * (m1-m2);
+        else if(y1>y2) {
+            return 10000;
         }
-
+        else {
         return 0;
-
+        }
     }
 };
 
