a89a95a19704450b50c6c60b06c435224d88f3e9
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-05-16 18:49:32 +0530

    my logic

---
+++ HACKERRANK/cavity_map.cpp
@@ -0,0 +1,26 @@
+// if the 9 is not a border then it is a cavity replace with x
+//
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+    private:
+    vector<string> cavityMap(vector<string> grid,int n) {
+
+
+        for(int i =0;i<n;i++) {
+            for(int j =0;j<n;j++) {
+                if(grid[i][j] != grid[j][i]) {
+                    if(grid[i][j] == '9') {
+                        grid[i][j] = 'X';
+                    }
+                }
+            }
+        }
+
+
+    }
+}; 
