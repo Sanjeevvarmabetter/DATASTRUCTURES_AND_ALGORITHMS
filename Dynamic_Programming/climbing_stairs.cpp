5849449f3a2b96743845571a7829a340ea351b4b
Sanjeevvarmabetter <sanjeevvarmacode@gmail.com>
2024-07-07 00:11:20 +0530

    added dp

---
+++ Dynamic_Programming/climbing_stairs.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// n = 2
+//
+// either 1 or 2
+//
+// ans = 2
+//
+
+
+int climb(int n) {
+	if(n<=1) {
+		return 1;
+	}
+
+	return climb(n-1) + climb(n-2);
+}
+
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	int ans = climb(n);
+	cout << ans << endl;
+	return 0;
+}
+
---
+++ Dynamic_Programming/fibonacci.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ int dynamic_topdown(int n,vector<int>&dp) {
+	 if(n<=1) {
+		 return n;
+	 }
+	
+ 	// i want the store also
+	//
+	if(dp!=-1) {
+		return dp[n];
+	}
+	
+	 // the formula
+
+ 	 return dp[n] = dynamic(n-1) + dynamic(n+2);
+
+ }
+
+int dynamic_bottomup(int n,vector<int>&dp) {
+	
+	dp[0] = 0;
+	dp[1] = 1;
+
+	for(int i=2;i<=n;i++) {
+
+		//formula
+		dp[i] = dp[i-1] + dp[i-2];
+	}
+
+	return dp[i];
+
+}
+
+	
+
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> dp(n+1,-1); // initially all the elements are -1
+	cout << dynamic(n,sp);
+	return 0;
+
+}
+
+
